f22f6de7ad046a2729fec9e5b723ff0fc81dd010
MingJam <162523837+Mingjaam@users.noreply.github.com>
2024-12-03 15:22:45 +0900

    16

--- 16-interpolationSearch/16-interpolationSearch.c
+++ 16-interpolationSearch/16-interpolationSearch.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 10000
-#define arr_size 1000
+#define numRange 10000
+#define arrSize 1000
 
-
-
+// 배열의 처음과 끝 일부를 출력하는 함수
 void printArray(int *array) {
     printf("Array Sorting Result:\n");
     for(int i = 0; i < 20; i++)
         printf("%4d ", array[i]);
     printf("\n");
-    for(int i = arr_size-20; i < arr_size; i++)
+    for(int i = arrSize-20; i < arrSize; i++)
         printf("%4d ", array[i]);
     printf("\n\n");
 } 
 
+// 배열에 랜덤한 값을 생성하여 채우는 함수
 void generateRandomArray(int *array) {
-    for(int i = 0; i < arr_size; i++) {
-        array[i] = rand() % 10000; 
+    for(int i = 0; i < arrSize; i++) {
+        array[i] = rand() % numRange; 
     }
 }
 
-
+// 두 정수의 값을 교환하는 함수
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// 퀵소트 파티션 함수
+// 퀵소트의 파티션을 수행하는 함수
 int partition(int *array, int low, int high) {
     int pivot = array[high];
     int i = low - 1;
@@ -48,7 +48,7 @@ int partition(int *array, int low, int high) {
     return i + 1;
 }
 
-// 퀵소트 구현
+// 퀵소트를 구현하는 함수
 void QuickSort(int *array, int low, int high) {
     if(low < high) {
         int pivtIndex = partition(array, low, high);
@@ -57,9 +57,9 @@ void QuickSort(int *array, int low, int high) {
     }
 }
 
+// 이진 탐색을 수행하여 비교 횟수를 반환하는 함수
 int binarySearch(int *array, int target) {
-
-    int left = 0, right = arr_size - 1;
+    int left = 0, right = arrSize - 1;
     int compareCount = 0;
     
     while(left <= right) {
@@ -72,8 +72,9 @@ int binarySearch(int *array, int target) {
     return compareCount;
 }
 
+// 보간 탐색을 수행하여 비교 횟수를 반환하는 함수
 int interpolationSearch(int *array, int target) {
-    int low = 0, high = arr_size - 1;
+    int low = 0, high = arrSize - 1;
     int compareCount = 0;
     
     while(low <= high && target >= array[low] && target <= array[high]) {
@@ -94,31 +95,35 @@ int interpolationSearch(int *array, int target) {
     return compareCount;
 }
 
+// 이진 탐색의 평균 비교 횟수를 계산하는 함수
 float getAverageBinarySearchCompareCount(int *array) {
     int totalCompares = 0;
-    for(int i = 0; i < arr_size; i++) {
-        int target = array[rand() % arr_size];
+    for(int i = 0; i < arrSize; i++) {
+        int target = array[rand() % arrSize];
         totalCompares += binarySearch(array, target);
     }
-    return (float)totalCompares / arr_size;
+    return (float)totalCompares / arrSize;
 }
 
+// 보간 탐색의 평균 비교 횟수를 계산하는 함수
 float getAverageInterpolationSearchCompareCount(int *array) {
     int totalCompares = 0;
-    for(int i = 0; i < arr_size; i++) {
-        int target = array[rand() % arr_size];
+    for(int i = 0; i < arrSize; i++) {
+        int target = array[rand() % arrSize];
         totalCompares += interpolationSearch(array, target);
     }
-    return (float)totalCompares / arr_size;
+    return (float)totalCompares / arrSize;
 }
+
+
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
-    int array[arr_size];
+    int array[arrSize];
     generateRandomArray(array);
 
 
-    QuickSort(array, 0, arr_size-1);
+    QuickSort(array, 0, arrSize-1);
     printArray(array);
 
     printf("Average Compare Count of Binary Search: %.2f\n",
